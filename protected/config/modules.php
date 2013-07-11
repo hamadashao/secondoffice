7c6b4de3c3834e7b771e4bd851364042e3c6682a
murusu <murusu0.0@gmail.com>
2013-07-11 16:45:01 +0800

    update
    Signed-off-by: murusu <murusu0.0@gmail.com>

---
+++ protected/config/modules.php
@@ -0,0 +1,20 @@
+<?php
+
+return searchForModules();
+
+function searchForModules(){
+
+	//$arr = array();
+
+	/* add your modules dynamically here */
+	
+	$arr = array(
+		// uncomment the following to enable the Gii tool
+		
+		'test',
+	);
+
+	return $arr;
+}
+
+?>
